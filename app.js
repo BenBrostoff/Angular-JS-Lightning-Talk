b4098c252d16a6e88232560e0fa5ad108960d8a3
Ben Brostoff <ben.brostoff@gmail.com>
2014-09-11 11:12:27 -0400

    Release and sign stars

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Wrap in anonymous function and call immediately
 (function(){
   // Declare module
   var app = angular.module('starCreate', []);
@@ -6,13 +7,29 @@
   // Controllers are capital case
   app.controller('StarController', function() {
     this.stars = stars;
+    this.sign = function(star) { 
+      star.signStar();
+      console.log(star);
+    };
+    this.release = function(star){
+      star.releaseStar();
+    }
   });
 
   var Star = function(name, age, quote) {
     this.name = name;
     this.age = age;
     this.quote = quote;
+    this.signed = false;
   };
+
+  Star.prototype.signStar = function() {
+    this.signed = true;
+  }
+
+  Star.prototype.releaseStar = function() {
+    this.signed = false;
+  }
 
   var stars = [ new Star("Adam Sandler", 48, "You can do it!"),
                 new Star("Matt Damon", 43, "Do you like apples?" )];
