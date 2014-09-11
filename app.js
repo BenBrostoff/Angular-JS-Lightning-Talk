8dbc513ff423f95c94749580730bc3f7011a2649
Ben Brostoff <ben.brostoff@gmail.com>
2014-09-11 10:57:32 -0400

    Actors rendering

---
+++ app.js
@@ -0,0 +1,23 @@
+var x = function(){
+  // Declare module
+  var app = angular.module('starCreate', []);
+
+  // Declare controller
+  // Controllers are capital case
+  app.controller('StarController', function() {
+    this.stars = stars;
+  });
+
+  var Star = function(name, age, quote) {
+    this.name = name;
+    this.age = age;
+    this.quote = quote;
+  };
+
+  var stars = [ new Star("Adam Sandler", 48, "You can do it!"),
+                new Star("Matt Damon", 43, "Do you like apples?" )];
+
+
+};
+
+x();
