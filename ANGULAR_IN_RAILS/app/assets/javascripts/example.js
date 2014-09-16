2bec09cb9841988890c7a9aa9e4b0a815e0e98b9
Ben Brostoff <ben.brostoff@gmail.com>
2014-09-16 10:06:51 -0400

    weatherService functional and got Fahrenheit function to execute

--- ANGULAR_IN_RAILS/app/assets/javascripts/example.js
+++ ANGULAR_IN_RAILS/app/assets/javascripts/example.js
@@ -1,23 +1,33 @@
 (function() {
   var app = angular.module('example', []);
 
-  app.controller('getWeather', ['$http', function($http) {
+  app.factory('weatherService', function(){
+    var fact = {};
+    fact.toFah = function(kelvin) {
+      return Math.floor(1.8 * (kelvin - 273) + 32);
+    }
+    return fact; 
+  });
+
+  app.controller('getWeather', ['$http', 'weatherService', function($http, weatherService) {
 
     this.city = "Boston";
+
     this.toF = function(kelvin) {
-      return Math.floor(1.8 * (kelvin - 273) + 32);
+      return weatherService.toFah(kelvin);
     }
 
     var weather = this;
     this.temperature = {};
-
+    this.desc = {};
 
     var weatherURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + this.city;
 
     $http.get(weatherURL).success(function(data) {
+      console.log("success ")
       weather.temperature = weather.toF(data["main"]["temp"]);
       weather.desc = data["weather"][0]["description"];
     });
-
   }]);
+
 })();
