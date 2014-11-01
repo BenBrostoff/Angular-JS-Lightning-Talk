fb8365df8e94d66586ef6f43af320a5d5acfe144
Ben Brostoff <ben.brostoff@gmail.com>
2014-11-01 19:44:09 -0400

    Basic game logic in place

--- ANGULAR_IN_RAILS/app/assets/javascripts/game.js
+++ ANGULAR_IN_RAILS/app/assets/javascripts/game.js
@@ -3,15 +3,45 @@
 
   game.controller('playGame', ["$scope", "$interval", function($scope, $interval) {
 
-    var sounds = ["Alpha", "Beta", "Epsilion", "Red Seventy", "Tranquility", "Blue Bird",
-                  "Piper", "G Force"];
+    this.answer = false
+    var sounds = ["Alpha", "Beta", "Epsilion"];
     var soundKey = {}
 
-    this.sayHello = function() {
-      var msg = new SpeechSynthesisUtterance('Hello World');
-      window.speechSynthesis.speak(msg);
+
+    formattedKey = ""
+
+    var scramble = function() {
+      var soundMix = Shuffle(sounds)
+      var keyMix = Shuffle(soundMix)
+      for(i = 0; i < 3; i++) {
+        soundKey[soundMix[i]] = i
+        formattedKey += keyMix[i] + " => " + i + " || "
+      }
+    }()
+
+    answerCode = ""
+    for(i = 0; i < 3; i++) {
+      answerCode += Shuffle(sounds)[i] += "      ."
     }
 
+    close = this;
+
+    this.answerCode = soundKey;
+
+    this.sayHello = function() {
+      var msg = new SpeechSynthesisUtterance(answerCode);
+      window.speechSynthesis.speak(msg);
+      $('#answer')[0].innerHTML = formattedKey;  
+      
+      this.hideAnswer()
+    }
+
+
+    this.hideAnswer = function() {
+      window.setTimeout(function(){
+        $('#answer')[0].innerHTML = '';  
+        }, 3000)
+    }
 
   }]);
 
---
+++ ANGULAR_IN_RAILS/app/assets/javascripts/shuffle.js
@@ -0,0 +1,4 @@
+function Shuffle(o){ //v1.0
+    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+};
