24e5cff7616fd45c5a8fe68dc92187b6afcba75e
Ben Brostoff <ben.brostoff@gmail.com>
2014-11-01 20:06:02 -0400

    Prompt success/fail on input

--- ANGULAR_IN_RAILS/app/assets/javascripts/game.js
+++ ANGULAR_IN_RAILS/app/assets/javascripts/game.js
@@ -1,39 +1,57 @@
 (function() {
   var game = angular.module('game', []);
 
-  game.controller('playGame', ["$scope", "$interval", function($scope, $interval) {
+  game.controller('playGame', function() {
 
-    this.answer = false
+    this.answer = false;
     var sounds = ["Alpha", "Beta", "Epsilion"];
-    var soundKey = {}
+    var soundKey = {};
+    var answerCodeArray = []
+    var formattedKey = "";
+    var answerCode = "";
+    var correctAnswer = "";
+    var close = this;
 
-
-    formattedKey = ""
-
-    var scramble = function() {
-      var soundMix = Shuffle(sounds)
-      var keyMix = Shuffle(soundMix)
-      for(i = 0; i < 3; i++) {
-        soundKey[soundMix[i]] = i
-        formattedKey += keyMix[i] + " => " + i + " || "
-      }
-    }()
-
-    answerCode = ""
-    for(i = 0; i < 3; i++) {
-      answerCode += Shuffle(sounds)[i] += "      ."
+    var reset = function() {
+      soundKey = {};
+      answerCodeArray = []
+      formattedKey = "";
+      answerCode = "";
+      close = this;
     }
 
-    close = this;
+    var Initialize = function() {
+      correctAnswer = "";
 
-    this.answerCode = soundKey;
+      var scramble = function() {
+        var soundMix = Shuffle(sounds)
+        var keyMix = Shuffle(soundMix)
+        for(i = 0; i < 3; i++) {
+          soundKey[soundMix[i]] = i
+          formattedKey += keyMix[i] + " => " + i + " || "
+        }
+      }()
+
+      for(i = 0; i < 3; i++) {
+        answerCodeArray.push(Shuffle(sounds)[i])
+        answerCode += answerCodeArray[i] + "  ."
+        correctAnswer += soundKey[answerCodeArray[i]]
+      }
+
+      close.answerCode = soundKey;
+      close.correctAnswer = correctAnswer;
+
+    }
 
     this.sayHello = function() {
+      Initialize(); 
+      console.log(correctAnswer);
       var msg = new SpeechSynthesisUtterance(answerCode);
       window.speechSynthesis.speak(msg);
       $('#answer')[0].innerHTML = formattedKey;  
       
-      this.hideAnswer()
+      this.hideAnswer();
+      reset();
     }
 
 
@@ -41,9 +59,19 @@
       window.setTimeout(function(){
         $('#answer')[0].innerHTML = '';  
         }, 3000)
-    }
+    };
 
-  }]);
+    this.checkAnswer = function(userAnswer) {
+      console.log(correctAnswer);
+      console.log(userAnswer);
+      if (correctAnswer == userAnswer) {
+        alert("Great job!")
+      }
+      else {
+        alert("Nope.")
+      }
+    }
+  });
 
 
 })();
