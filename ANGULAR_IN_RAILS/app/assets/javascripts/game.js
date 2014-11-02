b88512865bddaadb80a51555d82b5c70c5a56b13
Ben Brostoff <ben.brostoff@gmail.com>
2014-11-01 21:24:57 -0400

    Post request on successful game

--- ANGULAR_IN_RAILS/app/assets/javascripts/game.js
+++ ANGULAR_IN_RAILS/app/assets/javascripts/game.js
@@ -1,42 +1,56 @@
 (function() {
   var game = angular.module('game', []);
 
-  game.controller('playGame', function() {
+  game.factory('gameService', function($http, $q){
+    var fact = {};
 
-    this.answer = false;
-    var sounds = ["Alpha", "Beta", "Epsilion"];
+    fact.sendGame = function(score){
+      console.log(score);
+      
+       $http({
+            url: "/log_score",
+            method: "POST",
+            data: {"score": score} 
+          });      
+    }
+
+    return fact; 
+  });
+
+  game.controller('playGame', ['gameService', function(gameService) {
+    close = this;
+
+    var sounds = ["Alpha", "Beta", "Epsilion", "Reddington", "Kangaroo",
+                  "Ellington", "Chronologic", "Keeper", "Shoptop", "Ruggles"];
     var soundKey = {};
     var answerCodeArray = []
     var formattedKey = "";
     var answerCode = "";
     var correctAnswer = "";
-    var close = this;
 
     var reset = function() {
       soundKey = {};
       answerCodeArray = []
       formattedKey = "";
       answerCode = "";
-      close = this;
     }
 
     var Initialize = function() {
+      close.diff = parseInt($("#difficulty").val())
       correctAnswer = "";
 
       var scramble = function() {
-        var soundMix = Shuffle(sounds)
+        var soundMix = Shuffle(sounds).slice(0, close.diff)
         var keyMix = Shuffle(soundMix)
-        for(i = 0; i < 3; i++) {
+        for(i = 0; i < close.diff; i++) {
           soundKey[soundMix[i]] = i
           formattedKey += keyMix[i] + " => " + i + " || "
         }
-      }()
-
-      for(i = 0; i < 3; i++) {
-        answerCodeArray.push(Shuffle(sounds)[i])
-        answerCode += answerCodeArray[i] + "  ."
-        correctAnswer += soundKey[answerCodeArray[i]]
-      }
+        for(i = 0; i < close.diff; i++) {
+          answerCodeArray.push(Shuffle(soundMix)[i])
+          answerCode += answerCodeArray[i] + "  ."
+          correctAnswer += soundKey[answerCodeArray[i]]
+        }}();
 
       close.answerCode = soundKey;
       close.correctAnswer = correctAnswer;
@@ -46,32 +60,32 @@
     this.sayHello = function() {
       Initialize(); 
       console.log(correctAnswer);
+
       var msg = new SpeechSynthesisUtterance(answerCode);
       window.speechSynthesis.speak(msg);
+
       $('#answer')[0].innerHTML = formattedKey;  
       
       this.hideAnswer();
       reset();
     }
 
-
+ 
     this.hideAnswer = function() {
       window.setTimeout(function(){
         $('#answer')[0].innerHTML = '';  
-        }, 3000)
+        }, 5000)
     };
 
     this.checkAnswer = function(userAnswer) {
-      console.log(correctAnswer);
-      console.log(userAnswer);
       if (correctAnswer == userAnswer) {
         alert("Great job!")
+        gameService.sendGame(close.diff);
       }
       else {
         alert("Nope.")
       }
     }
-  });
-
+  }]);
 
 })();
--- ANGULAR_IN_RAILS/app/controllers/game_controller.rb
+++ ANGULAR_IN_RAILS/app/controllers/game_controller.rb
@@ -1,9 +1,12 @@
 class GameController < ApplicationController
+  skip_before_filter  :verify_authenticity_token 
 
   def game
   end
 
   def log_score 
+    p params["score"]
+    render json: {}
   end
 
 end
